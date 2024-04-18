51131819bda2d6fcb69a588affb92c49f63606c3
Abhineet1144 <166810940+Abhineet1144@users.noreply.github.com>
2024-04-18 11:04:11 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,6 +1,5 @@
 import pickle
 import getpass
-#import questionary
 
 p = ''
 pc = None
@@ -8,10 +7,15 @@ c = 0
 fe = False
 li = False
 
+#checking if the file exist or not using try
 try:
     f = open('users.dat', 'rb')
+
+    #creating variables
     fe = True
     c = 0
+
+    #asking if the user wants to login or register
     mde = input("Do you want to login or register: ")
     while mde.lower() not in ['login', 'register']:
         c += 1
@@ -20,8 +24,12 @@ try:
         mde = input("Do you want to login or register: ")
 
     if mde.lower() == 'login':
+        #asking the user for their username
         un = input("Enter username: ")
         p = ''
+
+
+        #checking if the username exist
         while True:
             d = pickle.load(f)
             for i in d.keys():
@@ -33,6 +41,7 @@ try:
             if li:
                 break
 
+        #asking the username for password and verifying if it is correct
         up = getpass.getpass("Enter password(The pass might not be visible but is getting recorded): ")
         if p == up:
             print("Logged in successfully")
@@ -43,8 +52,11 @@ try:
 
     else:
         print("Welcome to registration")
+
+        #asking the user for username
         un = input("Enter username: ")
-        e = True
+
+        #appending all the existing usernames into a list
         uns = []
         try:
             while True:
@@ -55,10 +67,12 @@ try:
         except:           
             f.close()
             
+        #checking if there is another user with same username
         while un in uns:
             print("Username already exists!")
             un = input("Try another unsername: ")
 
+        #asking for the password and also asking to confirm it
         pss = getpass.getpass("Enter your password(The pass might not be visible but is getting recorded): ")
         while pc != pss:
             if c >= 1:
@@ -66,7 +80,9 @@ try:
             pc = getpass.getpass("Confirm your password: ")
             print('\n')
             c += 1
-        f = open("users.dat", 'wb')
+
+        #writing the data in users.dat file
+        f = open("users.dat", 'ab')
         pickle.dump({un:pss}, f)
 
 except:
